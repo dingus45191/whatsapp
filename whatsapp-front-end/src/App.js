cfa285c7a404dfedb7ff568f094264e51f30cdd4
Mubashir Hasan <mubashirhasan716@gmail.com>
2021-01-13 22:41:43 +0530

    ok now will display messages on front-end

--- whatsapp-front-end/src/App.js
+++ whatsapp-front-end/src/App.js
@@ -1,8 +1,36 @@
 import './App.css';
 import Sidebar from "./Sidebar";
 import Chat from "./Chat";
+import {useEffect, useState} from "react";
+import Pusher from 'pusher-js';
+import axios from "./axios";
 
 function App() {
+    const [messages,setMessages]= useState([])
+    useEffect(()=>{
+        axios.get('/messages/sync').then(response =>{
+            setMessages(response.data)
+        })
+
+    },[])
+    useEffect(()=>{
+        const pusher = new Pusher('a1594a87ba3826b5f6e3', {
+            cluster: 'mt1'
+        });
+
+        const channel = pusher.subscribe('messages');
+        channel.bind('inserted', function(data) {
+            alert(JSON.stringify(data));
+            setMessages(...messages, data)
+        });
+        return()=>{
+            channel.unbind_all()
+            channel.unsubscribe()
+        }
+    },[messages]);
+
+
+    console.log(messages)
   return (
     <div className="app">
         <div className="app__body">
